f9042a864fe98290e3200936ba3dc1925ffd0b76
naizo10 <naizo018767@gmail.com>
2024-10-30 20:05:48 +0900

    Refactor Inheritance contract deployment method

--- contracts/script/deploy/DeployInheritanceScript.sol
+++ contracts/script/deploy/DeployInheritanceScript.sol
@@ -6,10 +6,7 @@ import {InheritanceDeployer} from "./InheritanceDeployer.sol";
 
 contract DeployInheritanceScript is MCScript {
     function run() public startBroadcastWith("DEPLOYER_PRIV_KEY") {
-        bytes32 hash = 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef; // 例としてのハッシュ値
-        uint256 lockTime = 90 days; // 例としてのロック期間
-
-        address _inheritance = InheritanceDeployer.deployInheritance(mc, hash, lockTime);
-        _saveAddrToEnv(_inheritance, "INHERITANCE_PROXY_ADDR_");
+        address dictionaryAddress = InheritanceDeployer.deployDictionaryInheritance(mc);
+        _saveAddrToEnv(dictionaryAddress, "INHERITANCE_DICTIONARY_ADDR_");
     }
 }
