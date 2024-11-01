95d73ef28e2e474694d855b8ef0993896146bdfc
pepetoma <blockchainjujisha@gmail.com>
2024-11-01 19:21:53 +0900

    addApprovedTokens関数の修正

--- contracts/src/Inheritance/functions/addApprovedTokens.sol
+++ contracts/src/Inheritance/functions/addApprovedTokens.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.23;
 
 import {Storage} from "bundle/inheritance/storage/Storage.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract addApprovedTokens {
     /**
@@ -9,6 +10,12 @@ contract addApprovedTokens {
      * @param _tokens The list of token addresses to approve
      */
     function addApprovedTokens(address[] calldata _tokens) external {
-        // Function implementation goes here
+        Schema.InheritanceState storage state = Storage.InheritanceState();
+        
+        for (uint256 i = 0; i < _tokens.length; i++) {
+            address token = _tokens[i];
+            IERC20(token).approve(address(this), type(uint256).max);
+            state.approvedTokens.push(token);
+        }
     }
 }
