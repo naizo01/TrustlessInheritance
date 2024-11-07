08d244c4f5f1e6aee8e5c8aa00d7691cf4b63759
naizo10 <naizo018767@gmail.com>
2024-11-07 21:36:47 +0900

    Revert "Add Landing page (src/app/page.tsx)"
    This reverts commit 2305b6873bce11aeb0c36b9ded4e763d2e633310.

--- frontend/src/app/beneficiary/layout.js
+++
@@ -1,9 +0,0 @@
-export default function Layout({ children }) {
-  return (
-    <div className="h-full gap-12">
-      <div className="">{children}</div>
-    </div>
-  );
-}
-
-// This is a nested layout within the RootLayout, where this Layout comes in.
