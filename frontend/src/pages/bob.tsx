f8aec32e562c7899ce158cf442373d6d2983e7fd
jirokk <kakaijiro@gmail.com>
2024-11-09 18:53:44 +0900

    Add bob-landing-page.jsx

--- frontend/src/pages/bob.tsx
+++ frontend/src/pages/bob.tsx
@@ -2,7 +2,8 @@
 
 // pages/index.tsx
 import { InheritanceUi } from "@/components/inheritance-ui";
+import BobLandingPage from "@/components/bob-landing-page";
 
 export default function Home() {
-  return <InheritanceUi />;
+  return <BobLandingPage />;
 }
