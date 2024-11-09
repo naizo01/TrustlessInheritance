71b9a4281af6848384ad577cd3a0ac20dbbf057a
jirokk <kakaijiro@gmail.com>
2024-11-10 07:23:48 +0900

    Add bob-inheritance-asset.tsx where Bob can view his inheritance

--- frontend/src/pages/bob.tsx
+++ frontend/src/pages/bob.tsx
@@ -5,7 +5,8 @@ import { InheritanceUi } from "@/components/inheritance-ui";
 import BobLandingPage from "@/components/bob-landing-page";
 import AddressInputPage from "@/components/bob-address-input";
 import ConfirmAddressesPage from "@/components/bob-confirm-addresses";
+import InheritanceAssetsPage from "@/components/bob-inheritance-assets";
 
 export default function Home() {
-  return <ConfirmAddressesPage />;
+  return <InheritanceAssetsPage />;
 }
