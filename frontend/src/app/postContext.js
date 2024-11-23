edf670dcdc09ec01f5296c1eb30d3df8d745e0be
jirokk <kakaijiro@gmail.com>
2024-11-23 22:33:07 +0900

    Add the global state PostContest to communicate between Alice and Bob

--- frontend/src/app/postContext.js
+++ frontend/src/app/postContext.js
@@ -1,15 +1,37 @@
-"use client";
-
 import { createContext, useContext, useState } from "react";
 
-const PostContext = createContext();
+// context
+const PostContext = createContext(null);
+
+// demo data
+const dummyTransactions = [
+  {
+    address: "0x0",
+    tokens: {},
+    lockPeriodEndDate: null,
+    lockPeriod: 3,
+  },
+];
+
+const dummyPositions = [
+  {
+    address: "0x0",
+    token: {
+      ETH: "100000000000",
+      USDT: "200",
+      USDC: "500",
+      DAI: "100000",
+    },
+  },
+];
 
 function PostProvider({ children }) {
+  const [positions, setPositions] = useState(dummyPositions);
+  const [transactions, setTransactions] = useState(dummyTransactions);
+
   return (
     <PostContext.Provider
-      value={{
-        transactions: [],
-      }}
+      value={{ positions, setPositions, transactions, setTransactions }}
     >
       {children}
     </PostContext.Provider>
@@ -18,7 +40,7 @@ function PostProvider({ children }) {
 
 function usePosts() {
   const context = useContext(PostContext);
-  if (context === undefined)
+  if (!context)
     throw new Error("PostContest was used outside of the PostProvider");
   return context;
 }
