9465c97cb2c508429c0079d1c8a6e30c12abec52
naizo10 <naizo018767@gmail.com>
2024-11-27 01:28:14 +0900

    zk

--- contracts/src/Inheritance/functions/zk/Groth16Verifier.sol
+++ contracts/src/Inheritance/functions/zk/Groth16Verifier.sol
@@ -59,7 +59,7 @@ contract Groth16Verifier {
 
     uint16 constant pLastMem = 896;
 
-    function verifyProof(uint[2] calldata _pA, uint[2][2] calldata _pB, uint[2] calldata _pC, uint[2] calldata _pubSignals) public view returns (bool) {
+    function verifyProof(uint[2] memory _pA, uint[2][2] memory _pB, uint[2] memory _pC, uint[2] memory _pubSignals) public view returns (bool) {
         assembly {
             function checkField(v) {
                 if iszero(lt(v, r)) {
