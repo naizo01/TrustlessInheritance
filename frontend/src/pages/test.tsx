5d2d3af0fb76d78cde9a78e93b3500d61d7f5a8a
jirokk <kakaijiro@gmail.com>
2024-11-28 14:06:31 +0900

    Remove demo data in bob-address-input.jsx

--- frontend/src/pages/test.tsx
+++ frontend/src/pages/test.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import TokenBalances from '@/components/sample/TokenBalances';
-import ApproveToken from '@/components/sample/ApproveToken';
+import React from "react";
+import TokenBalances from "@/components/sample/TokenBalances";
+import ApproveToken from "@/components/sample/ApproveToken";
 
 export default function Home() {
   return (
     <div>
-      <div style={{ border: '1px solid #ccc', padding: '10px', marginBottom: '20px' }}>
+      <div
+        style={{
+          border: "1px solid #ccc",
+          padding: "10px",
+          marginBottom: "20px",
+        }}
+      >
         <TokenBalances />
       </div>
-      <div style={{ border: '1px solid #ccc', padding: '10px' }}>
+      <div style={{ border: "1px solid #ccc", padding: "10px" }}>
         <ApproveToken />
       </div>
     </div>
