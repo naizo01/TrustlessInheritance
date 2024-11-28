e2f46a68b9621d244620ed393a93d09b710f4e49
naizo10 <naizo018767@gmail.com>
2024-11-28 13:29:46 +0900

    Add deployment scripts for contract integration

---
+++ contracts/script/deploy/sh/01.sh
@@ -0,0 +1,19 @@
+
+# .envファイルを読み込む
+if [ -f .env ]; then
+  export $(cat .env | xargs)
+else
+  echo ".envファイルが見つかりません。"
+  exit 1
+fi
+
+# forge scriptコマンドを実行
+forge script script/deploy/01_DeployInheritanceScript.sol:DeployInheritanceScript \
+  --rpc-url $RPC_URL \
+  --chain-id $CHAIN_ID \
+  --private-key $PRIVATE_KEY \
+  --verifier-url $SCAN_URL \
+  --verify \
+  --etherscan-api-key $SCAN_API_KEY \
+  --broadcast \
+  -vvvv
