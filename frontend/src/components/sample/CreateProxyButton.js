00caea8addc4b5adad2e2642ca9f95cf8fe92ac3
naizo10 <naizo018767@gmail.com>
2024-11-28 13:57:44 +0900

    Refactor input fields and add labels in CreateProxyButton

--- frontend/src/components/sample/CreateProxyButton.js
+++ frontend/src/components/sample/CreateProxyButton.js
@@ -10,7 +10,7 @@ const CreateProxyButton = () => {
   const { writeContract, waitFn } = useCreateProxy(hash, lockTime);
 
   const handleHashInput = async (value) => {
-    const hash = await calculatePoseidonHash(value)
+    const hash = await calculatePoseidonHash(value);
     setHash(hash);
   };
 
@@ -20,22 +20,32 @@ const CreateProxyButton = () => {
 
   return (
     <div>
-      <input
-        type="number"
-        value={inputValue}
-        onChange={(e) => {
-          const value = Number(e.target.value);
-          setInputValue(value);
-          handleHashInput(value);
-        }}
-        placeholder="Enter value to hash"
-      />
-      <input
-        type="number"
-        value={lockTime.toString()}
-        onChange={(e) => setLockTime(BigInt(e.target.value))}
-        placeholder="Enter lock time"
-      />
+      <div>
+        <label>
+          Value to Hash:
+          <input
+            type="number"
+            value={inputValue}
+            onChange={(e) => {
+              const value = Number(e.target.value);
+              setInputValue(value);
+              handleHashInput(value);
+            }}
+            placeholder="Enter value to hash"
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Lock Time:
+          <input
+            type="number"
+            value={lockTime.toString()}
+            onChange={(e) => setLockTime(BigInt(e.target.value))}
+            placeholder="Enter lock time"
+          />
+        </label>
+      </div>
       <button onClick={handleCreateProxy}>Create Proxy</button>
       {waitFn.isLoading && <p>Transaction is processing...</p>}
       {waitFn.isSuccess && <p>Proxy created successfully!</p>}
