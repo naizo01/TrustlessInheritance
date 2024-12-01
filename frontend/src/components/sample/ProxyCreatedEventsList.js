341e0c337d12d6704f639f585c5fb84ce1b87140
naizo10 <naizo018767@gmail.com>
2024-12-01 12:37:40 +0900

    Update styling for ProxyCreatedEventsList component

--- frontend/src/components/sample/ProxyCreatedEventsList.js
+++ frontend/src/components/sample/ProxyCreatedEventsList.js
@@ -14,23 +14,28 @@ const ProxyCreatedEventsList = () => {
   };
 
   return (
-    <div>
-      <h1>Proxy Created Events</h1>
-      <button onClick={handleCreateProxy}>getProxyCreatedEvents</button>
-      <table>
+    <div style={{ padding: '20px', borderRadius: '5px' }}>
+      <h1 style={{ color: 'black' }}>Proxy Created Events</h1>
+      <button 
+        onClick={handleCreateProxy} 
+        style={{ backgroundColor: 'white', color: 'black', border: '1px solid black', borderRadius: '5px', padding: '10px 20px', cursor: 'pointer' }}
+      >
+        getProxyCreatedEvents
+      </button>
+      <table style={{ width: '100%', borderCollapse: 'collapse', marginTop: '20px' }}>
         <thead>
           <tr>
-            <th>owner</th>
-            <th>proxy address</th>
-            <th>hush</th>
+            <th style={{ border: '1px solid black', padding: '10px', color: 'black' }}>owner</th>
+            <th style={{ border: '1px solid black', padding: '10px', color: 'black' }}>proxy address</th>
+            <th style={{ border: '1px solid black', padding: '10px', color: 'black' }}>hush</th>
           </tr>
         </thead>
         <tbody>
           {events.map((event, index) => (
             <tr key={index}>
-              <td>{event.args[0]}</td>
-              <td>{event.args[1]}</td>
-              <td>{event.args[2].toString()}</td>
+              <td style={{ border: '1px solid black', padding: '10px', color: 'black' }}>{event.args[0]}</td>
+              <td style={{ border: '1px solid black', padding: '10px', color: 'black' }}>{event.args[1]}</td>
+              <td style={{ border: '1px solid black', padding: '10px', color: 'black' }}>{event.args[2].toString()}</td>
             </tr>
           ))}
         </tbody>
