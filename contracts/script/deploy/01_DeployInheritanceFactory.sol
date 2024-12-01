dd78f73d38c2df98bb16a198c33e183a5acc46f0
naizo10 <naizo018767@gmail.com>
2024-12-01 18:17:03 +0900

    Update contracts to dynamically generate env variables

--- contracts/script/deploy/01_DeployInheritanceFactory.sol
+++ contracts/script/deploy/01_DeployInheritanceFactory.sol
@@ -5,13 +5,16 @@ import {MCScript, MCDevKit} from "@mc-devkit/Flattened.sol";
 import "bundle/inheritance/InheritanceFactory.sol";
 
 contract DeployInheritanceFactory is MCScript {
-    function run() external  startBroadcastWith("PRIVATE_KEY") {
-        address zk = vm.envAddress("ZK_ADDR_84532");
+    function run() external startBroadcastWith("PRIVATE_KEY") {
+        uint256 chainId = vm.envUint("CHAIN_ID");
+        string memory zkEnvVar = string(
+            abi.encodePacked("ZK_ADDR_", vm.toString(chainId))
+        );
+        address zk = vm.envAddress(zkEnvVar);
 
         // InheritanceFactoryコントラクトのデプロイ
         InheritanceFactory factory = new InheritanceFactory(zk);
 
         _saveAddrToEnv(address(factory), "INHERITANCE_DACTORY_ADDR_");
-
     }
 }
